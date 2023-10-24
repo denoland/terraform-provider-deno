fa5e2e8f1c5ddb63178985d7eeccb0ebc1b5e6c2
Yusuke Tanaka <yusuktan@maguro.dev>
2023-10-24 16:12:49 +0900

    path join no longer needed

--- internal/provider/testdata/binary/main.ts
+++ internal/provider/testdata/binary/main.ts
@@ -1,9 +1,6 @@
-import { dirname, join } from "https://deno.land/std@0.203.0/url/mod.ts";
-
 Deno.serve(async () => {
-  const url = join(dirname(import.meta.url), "computer_screen_programming.png");
   try {
-    const image = await Deno.readFile(url);
+    const image = await Deno.readFile("computer_screen_programming.png");
     return new Response(image);
   } catch (error) {
     return new Response(error.message);
